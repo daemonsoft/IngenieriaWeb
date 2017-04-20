80cc669ad4693cee9ce54ad1ad305ac5aa6e96d3
daemonsoft <daemonsoft@gmail.com>
2017-04-20 13:40:31 -0500

    Agregado Projecto de Ejemplo jersey

---
+++ WebProject/src/com/ingweb/ws/PrimerEjemplo.java
@@ -0,0 +1,33 @@
+package com.ingweb.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("saludo")
+public class PrimerEjemplo {
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String saludar() {
+		return "Hola";
+	}
+
+	@POST
+	@Path("save")
+	@Produces(MediaType.TEXT_HTML)
+	public String otroSaludo(@QueryParam("name") String name) {
+		return "Otro " + name;
+	}
+	
+	@POST
+	@Path("otro/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String otroSaludoPath(@PathParam("name") String name) {
+		return "Otro " + name;
+	}
+}
